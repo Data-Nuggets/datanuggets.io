2e9ac0fc73e1b9d185fd19af8200b11bb13070b9
Matsuuu <huhta.matias@gmail.com>
2022-03-21 22:33:43 +0200

    Stylized blog posts

--- site/src/components/blog-author.js
+++ site/src/components/blog-author.js
@@ -1,28 +1,32 @@
 import { html, css, LitElement } from 'lit';
 
 export class BlogAuthor extends LitElement {
-  static get properties() {
-    return {
-      name: { type: String },
-      href: { type: String },
-    };
-  }
+    static get properties() {
+        return {
+            name: { type: String },
+            href: { type: String },
+        };
+    }
 
-  static get styles() {
-    return css`
+    static get styles() {
+        return css`
       .author {
         margin-bottom: 0.75rem;
       }
+      a {
+          color: inherit;
+          text-decoration: none;
+      }
     `;
-  }
+    }
 
-  render() {
-    return html`
+    render() {
+        return html`
       <div class="author">
         <p><a href=${this.href}>${this.name}</a></p>
       </div>
     `;
-  }
+    }
 }
 
 customElements.define('blog-author', BlogAuthor);
